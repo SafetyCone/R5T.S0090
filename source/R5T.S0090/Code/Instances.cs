ba556c3acba08af645c079300eb718a6f79198ae
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:03 -0700

    Interim changes.

--- source/R5T.S0090/Code/Instances.cs
+++ source/R5T.S0090/Code/Instances.cs
@@ -5,6 +5,7 @@ namespace R5T.S0090
 {
     public static class Instances
     {
-		
+        public static Z0015.IFilePaths FilePaths => Z0015.FilePaths.Instance;
+        public static F0033.INotepadPlusPlusOperator NotepadPlusPlusOperator => F0033.NotepadPlusPlusOperator.Instance;
     }
 }
