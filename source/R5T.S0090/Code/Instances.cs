e8d3e6bdfbc7101c10e1399b6296e5feebda06c8
davidcoats <david@getsafetycone.com>
2023-10-18 10:46:56 -0700

    Interim changes.

--- source/R5T.S0090/Code/Instances.cs
+++ source/R5T.S0090/Code/Instances.cs
@@ -7,8 +7,8 @@ namespace R5T.S0090
         L0055.Instances
     {
         public static L0059.IAssemblyNameOperator AssemblyNameOperator => L0059.AssemblyNameOperator.Instance;
-        public static L0059.IExecutablePathOperator ExecutablePathOperator => L0059.ExecutablePathOperator.Instance;
-        public static L0059.IFileOperator FileOperator => L0059.FileOperator.Instance;
+        public static new L0059.IExecutablePathOperator ExecutablePathOperator => L0059.ExecutablePathOperator.Instance;
+        public static new L0059.IFileOperator FileOperator => L0059.FileOperator.Instance;
         public static L0059.IFileSystemOperator FileSystemOperator => L0059.FileSystemOperator.Instance;
         public static L0059.IPortableExecutableOperator PortableExecutableOperator => L0059.PortableExecutableOperator.Instance;
     }
